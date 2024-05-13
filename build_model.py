71db95958b7125f5e9541fb7e4fc8eee0b322356
AdamKuraczynski <kuraczynski.adam@gmail.com>
2024-05-13 23:50:49 +0200

    no comments, dir changes, cleanup

--- build_model.py
+++ build_model.py
@@ -1,27 +1,24 @@
-import numpy as np  # Importing NumPy library and aliasing it as 'np'
-import cv2  # Importing OpenCV library for computer vision tasks
-import os  # Importing os module for interacting with the operating system
+import numpy as np
+import cv2
+import os
 
-path = "C:/temp/DataDinousaurRecog/Dinosaurs"  # Assigning the path to the dataset directory to the variable 'path'
-categories = os.listdir(path)  # Listing all directories and files in the specified path and storing them in 'categories'
-categories.sort()  # Sorting the list of categories alphabetically
-print(categories)  # Printing the list of categories
+path = "dinosaursDataset"
+categories = os.listdir(path)
+categories.sort()
+print(categories)
 
-numOfClasses = len(categories)  # Getting the number of categories and storing it in 'numOfClasses'
-print("Number of categories :")  # Printing a message
-print(numOfClasses)  # Printing the number of categories
+numOfClasses = len(categories)
+print("Number of categories :")
+print(numOfClasses)
 
-########################################################
-batchSize = 32  # Setting the batch size to 32
-########################################################
-imageSize = (224,224)  # Defining the target size for images to (224, 224)
+batchSize = 32
+imageSize = (224,224)
 
-# Prepare the data
-from keras.preprocessing.image import ImageDataGenerator  # Importing ImageDataGenerator from Keras
+from keras.preprocessing.image import ImageDataGenerator
 
-datagen = ImageDataGenerator(rescale= 1./255, validation_split=0.2, horizontal_flip=True)  # Creating an ImageDataGenerator object with specified transformations
+datagen = ImageDataGenerator(rescale= 1./255, validation_split=0.2, horizontal_flip=True)
 
-train_dataset = datagen.flow_from_directory(batch_size=batchSize,  # Generating a data flow for training set from directory with specified parameters
+train_dataset = datagen.flow_from_directory(batch_size=batchSize,
                                            directory=path,
                                            color_mode='rgb',
                                            shuffle=True,
@@ -29,7 +26,7 @@ train_dataset = datagen.flow_from_directory(batch_size=batchSize,  # Generating 
                                            subset="training",
                                            class_mode="categorical")
 
-validation_dataset = datagen.flow_from_directory(batch_size=batchSize,  # Generating a data flow for validation set from directory with specified parameters
+validation_dataset = datagen.flow_from_directory(batch_size=batchSize,
                                            directory=path,
                                            color_mode='rgb',
                                            shuffle=True,
@@ -37,78 +34,73 @@ validation_dataset = datagen.flow_from_directory(batch_size=batchSize,  # Genera
                                            subset="validation",
                                            class_mode="categorical")
 
-batch_x, batch_y = next(train_dataset)  # Retrieving the next batch of data and labels from the training dataset
+batch_x, batch_y = next(train_dataset)
 
-# Print the shapes of the first patch (images and labels)
-print('Batch of images shape ', batch_x.shape)  # Printing the shape of batch of images
-print('Batch of images label ', batch_y.shape)  # Printing the shape of batch of labels
+print('Batch of images shape ', batch_x.shape)
+print('Batch of images label ', batch_y.shape)
 
-# Build the CNN model
-from keras.models import Sequential  # Importing Sequential model from Keras
-from keras.layers import Conv2D, MaxPooling2D, Dropout, Flatten, Dense  # Importing various layers from Keras
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D, Dropout, Flatten, Dense
 
-model = Sequential()  # Creating a Sequential model
-model.add(Conv2D(filters=16, kernel_size =3, activation='relu', padding='same', input_shape=(224,224,3)))  # Adding a convolutional layer to the model
-model.add(MaxPooling2D(pool_size=2))  # Adding a max pooling layer
-model.add(Dropout(0.5))  # Adding a dropout layer
+model = Sequential()
+model.add(Conv2D(filters=16, kernel_size =3, activation='relu', padding='same', input_shape=(224,224,3)))
+model.add(MaxPooling2D(pool_size=2))
+model.add(Dropout(0.5))
 
-model.add(Conv2D(filters=32, kernel_size =3, activation='relu', padding='same'))  # Adding another convolutional layer
-model.add(MaxPooling2D(pool_size=2))  # Adding another max pooling layer
+model.add(Conv2D(filters=32, kernel_size =3, activation='relu', padding='same'))
+model.add(MaxPooling2D(pool_size=2))
 
-model.add(Conv2D(filters=64, kernel_size =3, activation='relu', padding='same'))  # Adding another convolutional layer
-model.add(MaxPooling2D(pool_size=2))  # Adding another max pooling layer
-model.add(Dropout(0.5))  # Adding another dropout layer
+model.add(Conv2D(filters=64, kernel_size =3, activation='relu', padding='same'))
+model.add(MaxPooling2D(pool_size=2))
+model.add(Dropout(0.5))
 
-model.add(Flatten())  # Flattening the output from the convolutional layers
+model.add(Flatten())
 
-model.add(Dense(128, activation='relu'))  # Adding a fully connected layer
-model.add(Dropout(0.5))  # Adding a dropout layer
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.5))
 
-model.add(Dense(numOfClasses, activation='softmax'))  # Adding a softmax output layer with number of classes as output units
+model.add(Dense(numOfClasses, activation='softmax'))
 
-print(model.summary())  # Printing the summary of the model architecture
+print(model.summary())
 
-model.compile(loss="categorical_crossentropy", optimizer="Adam", metrics=["accuracy"])  # Compiling the model with specified loss function, optimizer, and metrics
+model.compile(loss="categorical_crossentropy", optimizer="Adam", metrics=["accuracy"])
 
-stepsPerEpochs = np.ceil(train_dataset.samples / batchSize)  # Calculating the number of steps per epoch for training
-validationSteps = np.ceil(validation_dataset.samples / batchSize)  # Calculating the number of validation steps
+stepsPerEpochs = np.ceil(train_dataset.samples / batchSize)
+validationSteps = np.ceil(validation_dataset.samples / batchSize)
 
-# Early stop
-from keras.callbacks import ModelCheckpoint  # Importing ModelCheckpoint callback from Keras
+from keras.callbacks import ModelCheckpoint
 
-best_model_file= "c/temp/dino.h5"  # Defining the file path to save the best model
-best_model = ModelCheckpoint(best_model_file, monitor='val_accuracy', verbose=1, save_best_only=True)  # Creating a callback to save the best model during training
+best_model_file= "trained_model/dino.h5"
+best_model = ModelCheckpoint(best_model_file, monitor='val_accuracy', verbose=1, save_best_only=True)
 
-history = model.fit(train_dataset,  # Training the model with specified parameters
+history = model.fit(train_dataset,
                     steps_per_epoch=stepsPerEpochs,
-                    epochs=50,
+                    epochs=10,
                     validation_data=validation_dataset,
                     validation_steps=validationSteps,
                     callbacks=[best_model] )
 
-import matplotlib.pyplot as plt  # Importing matplotlib for plotting
+import matplotlib.pyplot as plt
 
-acc = history.history['accuracy']  # Getting the accuracy values from training history
-val_acc = history.history['val_accuracy']  # Getting the validation accuracy values from training history
-loss = history.history['loss']  # Getting the loss values from training history
-val_loss = history.history['val_loss']  # Getting the validation loss values from training history
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
 
-epochsForGraph = range(len(acc))  # Generating a range of epochs for plotting
+epochsForGraph = range(len(acc))
 
-# Plot the train and validation
-plt.plot(epochsForGraph, acc, 'r', label='Train accuracy')  # Plotting training accuracy
-plt.plot(epochsForGraph, val_acc, 'b', label='Validation accuracy')  # Plotting validation accuracy
-plt.xlabel('Epochs')  # Labeling x-axis
-plt.ylabel('Accuracy')  # Labeling y-axis
-plt.title("Train and Validation Accuracy")  # Setting the title
-plt.legend(loc='lower right')  # Adding legend
-plt.show()  # Displaying the plot
+plt.plot(epochsForGraph, acc, 'r', label='Train accuracy')
+plt.plot(epochsForGraph, val_acc, 'b', label='Validation accuracy')
+plt.xlabel('Epochs')
+plt.ylabel('Accuracy')
+plt.title("Train and Validation Accuracy")
+plt.legend(loc='lower right')
+plt.show()
 
-# Plot loss and validation loss
-plt.plot(epochsForGraph, loss, 'r', label='Train loss')  # Plotting training loss
-plt.plot(epochsForGraph, val_loss, 'b', label='Validation loss')  # Plotting validation loss
-plt.xlabel('Epochs')  # Labeling x-axis
-plt.ylabel('Loss')  # Labeling y-axis
-plt.title("Train and Validation Loss")  # Setting the title
-plt.legend(loc='upper right')  # Adding legend
-plt.show()  # Displaying the plot
+plt.plot(epochsForGraph, loss, 'r', label='Train loss')
+plt.plot(epochsForGraph, val_loss, 'b', label='Validation loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.title("Train and Validation Loss")
+plt.legend(loc='upper right')
+plt.show()
--- test_model.py
+++ test_model.py
@@ -1,54 +1,47 @@
-import tensorflow as tf  # Importing TensorFlow library
-import os  # Importing os module for interacting with the operating system
-from keras.utils import img_to_array, load_img  # Importing utility functions from Keras for image conversion
-import numpy as np  # Importing NumPy library
-import cv2  # Importing OpenCV library for computer vision tasks
+import tensorflow as tf
+import os
+from keras.utils import img_to_array, load_img
+import numpy as np
+import cv2
 
-model = tf.keras.models.load_model("c/temp/dino.h5")  # Loading the trained model from the specified path
-print(model.summary())  # Printing the summary of the loaded model architecture
+model = tf.keras.models.load_model("trained_model/dino.h5")  
+print(model.summary())
 
-########################################################
-batchSize = 32  # Setting the batch size for prediction
-########################################################
+batchSize = 32
 
-# categories
-source_folder = "C:/temp/DataDinousaurRecog/Dinosaurs"  # Defining the path to the folder containing categories (dinosaur images)
-categories = os.listdir(source_folder)  # Listing all directories and files in the specified folder
-categories.sort()  # Sorting the list of categories alphabetically
-print(categories)  # Printing the list of categories
+source_folder = "dinosaursDataset"
+categories = os.listdir(source_folder)
+categories.sort()
+print(categories)
 
-numOfClasses = len(categories)  # Getting the number of categories and storing it in 'numOfClasses'
-print("Number of categories :")  # Printing a message
-print(numOfClasses)  # Printing the number of categories
+numOfClasses = len(categories)
+print("Number of categories :")
+print(numOfClasses)
 
 def prepareImage(pathForImage):
-    image = load_img(pathForImage, target_size=(224,224))  # Loading the image from the specified path and resizing it to (224,224)
-    imgResult = img_to_array(image)  # Converting the image to a NumPy array
-    imgResult = np.expand_dims(imgResult, axis=0)  # Adding an extra dimension to the array
-    imgResult = imgResult /255.  # Normalizing the pixel values
-    return imgResult  # Returning the preprocessed image
+    image = load_img(pathForImage, target_size=(224,224))
+    imgResult = img_to_array(image)
+    imgResult = np.expand_dims(imgResult, axis=0)
+    imgResult = imgResult /255.
+    return imgResult
 
-# run the prediction
-testImagPath = "C:/temp/DataDinousaurRecog/ptero_test.png"  # Defining the path to the test image
-imageForModel = prepareImage(testImagPath)  # Preparing the test image for prediction
+testImagPath = "ptero_test.png" 
+imageForModel = prepareImage(testImagPath)
 
-resultArray = model.predict(imageForModel, batch_size=batchSize, verbose=1)  # Making a prediction on the test image
+resultArray = model.predict(imageForModel, batch_size=batchSize, verbose=1)
 
-########################################################
-answers = np.argmax(resultArray, axis=1)  # Finding the index of the category with the highest probability
-########################################################
+answers = np.argmax(resultArray, axis=1)
 
-print(answers[0])  # Printing the index of the predicted category
+print(answers[0])
 
-text = categories[answers[0]]  # Getting the predicted category label based on the index
-print ("Predicted : "+ text)  # Printing the predicted category label
+text = categories[answers[0]]  
+print ("Predicted : "+ text)  
 
-# the image
-img = cv2.imread(testImagPath)  # Loading the test image using OpenCV
-font = cv2.FONT_HERSHEY_COMPLEX  # Choosing a font for text overlay
+img = cv2.imread(testImagPath)  
+font = cv2.FONT_HERSHEY_COMPLEX
 
-cv2.putText(img, text, (0,20), font, 0.5, (209,19,77), 2)  # Adding text overlay with the predicted category label
-cv2.imshow('img', img)  # Displaying the image with text overlay
-cv2.waitKey(0)  # Waiting for a key press to exit
+cv2.putText(img, text, (0,20), font, 0.5, (209,19,77), 2)
+cv2.imshow('img', img)  
+cv2.waitKey(0)
 
-cv2.destroyAllWindows()  # Closing all OpenCV windows
+cv2.destroyAllWindows()
